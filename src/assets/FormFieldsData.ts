5a909d5ad6f9c1e3af78ac6a41fe7ebbad200156
Michal Borowski <unpolacoo@gmail.com>
2020-10-01 15:22:13 +0200

    Create array with fields data

---
+++ src/assets/FormFieldsData.ts
@@ -0,0 +1,9 @@
+const fields = [
+    {name: 'startValue', labelText: 'Starting Amount'},
+    {name: 'additionalContribution', labelText: 'Additional Contribution'},
+    {name: 'frequencyContribution', labelText: 'Frequency Contribution'},
+    {name: 'timeContribution', labelText: 'Time Contribution'},
+    {name: 'interestRate', labelText: 'Estimated Interest Rate'},
+];
+
+export default fields;
