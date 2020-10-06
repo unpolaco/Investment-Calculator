2aec868d842d32dccbead439ef98ef0cf45c0b57
Michal Borowski <unpolacoo@gmail.com>
2020-10-07 00:25:27 +0200

    Add chartPie to ResultCard component

--- src/components/ResultsCard/ResultCard.tsx
+++ src/components/ResultsCard/ResultCard.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import {ChartBar} from './ResultChartBar';
+import {ChartPie} from './ResultChartPie';
 
-export default function ResultCard({data}: any) {
+export default function ResultCard({chartBarData, chartPieData}: any) {
     return (
-        <div style={{height: '200px'}}>
-            <ChartBar data={data} />
+        <div style={{height: '400px'}}>
+            <ChartBar data={chartBarData} />
+            <ChartPie data={chartPieData} />
         </div>
     );
 }
