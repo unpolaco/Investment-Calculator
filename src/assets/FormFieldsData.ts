c6bde9727b4b31cb1532e28fc929c5602b174aa3
Michal Borowski <unpolacoo@gmail.com>
2020-10-06 19:54:11 +0200

    Delete FormFieldsData file

--- src/assets/FormFieldsData.ts
+++
@@ -1,9 +0,0 @@
-const fields = [
-    {name: 'startValue', label: 'Starting Amount'},
-    {name: 'additionalContribution', label: 'Additional Contribution'},
-    {name: 'frequencyContribution', label: 'Frequency Contribution'},
-    {name: 'yearsContribution', label: 'Years to Accumulate'},
-    {name: 'returnRate', label: 'Expected Rate of Return'},
-];
-
-export default fields;
