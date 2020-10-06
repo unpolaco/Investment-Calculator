78d333a25848d0099e08de2d6e0186363b461078
Michal Borowski <unpolacoo@gmail.com>
2020-10-07 00:35:48 +0200

    Create LogIn and SignUp components

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,11 +1,16 @@
 import React from 'react';
 import CalculatorForm from './CalculatorForm/CalculatorForm';
+import {LogInForm} from './Header/LogInForm';
+import {SignUpForm} from './Header/SignUpForm';
 
 export const App = () => {
     return (
         <>
             <header>
                 <h3>Solid Investment Calculator!</h3>
+                <p>Welcome stranger!</p>
+                <LogInForm />
+                <SignUpForm />
             </header>
             <CalculatorForm />
         </>
