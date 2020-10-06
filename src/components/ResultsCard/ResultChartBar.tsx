c653effec69eb3a63971cf8e778406cac94b7ab3
Michal Borowski <unpolacoo@gmail.com>
2020-10-06 10:17:11 +0200

    Remove legend from chartbar

--- src/components/ResultsCard/ResultChartBar.tsx
+++ src/components/ResultsCard/ResultChartBar.tsx
@@ -4,8 +4,8 @@ import {ResponsiveBar} from '@nivo/bar';
 export const ChartBar = ({data}: any) => (
     <ResponsiveBar
         data={data}
-        keys={['startYearValue', 'cumulativeInterest', 'cumulativeContribution']}
-        indexBy="startYearValue"
+        keys={['startYearValue', 'cumulativeContribution', 'cumulativeInterest']}
+        indexBy="year"
         margin={{top: 50, right: 130, bottom: 50, left: 60}}
         padding={0.2}
         colors={{scheme: 'nivo'}}
@@ -15,7 +15,6 @@ export const ChartBar = ({data}: any) => (
             tickSize: 5,
             tickPadding: 5,
             tickRotation: 0,
-            legend: 'years',
             legendPosition: 'middle',
             legendOffset: 32,
         }}
@@ -23,35 +22,10 @@ export const ChartBar = ({data}: any) => (
             tickSize: 5,
             tickPadding: 5,
             tickRotation: 0,
-            legend: 'amount',
             legendPosition: 'middle',
             legendOffset: -40,
         }}
         enableLabel={false}
-        legends={[
-            {
-                dataFrom: 'keys',
-                anchor: 'bottom-right',
-                direction: 'column',
-                justify: false,
-                translateX: 120,
-                translateY: 0,
-                itemsSpacing: 2,
-                itemWidth: 100,
-                itemHeight: 20,
-                itemDirection: 'left-to-right',
-                itemOpacity: 0.85,
-                symbolSize: 20,
-                effects: [
-                    {
-                        on: 'hover',
-                        style: {
-                            itemOpacity: 1,
-                        },
-                    },
-                ],
-            },
-        ]}
         animate={true}
         motionStiffness={90}
         motionDamping={15}
