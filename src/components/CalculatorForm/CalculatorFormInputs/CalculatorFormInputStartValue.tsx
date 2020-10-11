e9cdf7707c94894b51a3a8f957047bd2b94ff114
Michal Borowski <unpolacoo@gmail.com>
2020-10-12 01:13:57 +0200

    Add example suffix to input

--- src/components/CalculatorForm/CalculatorFormInputs/CalculatorFormInputStartValue.tsx
+++ src/components/CalculatorForm/CalculatorFormInputs/CalculatorFormInputStartValue.tsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import {Field, useField} from 'formik';
-import {Input, Fieldset, Label, Error} from './CalculatorFormInputs.styles';
+import {Input, Fieldset, Label, Error, FieldContainer} from './CalculatorFormInputs.styles';
 
 export const CalculatorInputStartValue: React.FC = () => {
     const [field, meta] = useField('startValue');
     return (
         <Fieldset>
             <Label htmlFor={field.name}>Starting Amount</Label>
-            <Field autoFocus type="number" as={Input} {...field} />
+            <FieldContainer>
+                <Field autoFocus type="number" as={Input} {...field} />
+            </FieldContainer>
             {meta.error && <Error>{meta.error}</Error>}
         </Fieldset>
     );
--- src/components/CalculatorForm/CalculatorFormInputs/CalculatorFormInputs.styles.ts
+++ src/components/CalculatorForm/CalculatorFormInputs/CalculatorFormInputs.styles.ts
@@ -1,12 +1,34 @@
 import styled from '@emotion/styled';
 
+export const FieldContainer = styled.div`
+    display: inline-block;
+    position: relative;
+    ::after {
+        position: absolute;
+        top: 7px;
+        right: 15px;
+        content: 'zł';
+    }
+`;
+
 export const Input = styled.input`
     border-color: transparent;
-    padding: 5px;
+    padding: 5px 35px;
     height: 20px;
+    width: 120px;
     flex: 1;
     border: 1px solid rgba(0, 0, 0, 0.3);
     border-radius: 5px;
+    outline-color: transparent;
+    text-align: right;
+    ::-webkit-outer-spin-button,
+    ::-webkit-inner-spin-button {
+        -webkit-appearance: none;
+        margin: 0;
+    }
+    [type='number'] {
+        -moz-appearance: textfield;
+    }
 `;
 export const Fieldset = styled.fieldset`
     display: flex;
