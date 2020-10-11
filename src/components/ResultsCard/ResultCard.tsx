ed64965bf8ddbaaa0120af22ebde51ec189d949f
Michal Borowski <unpolacoo@gmail.com>
2020-10-11 02:29:48 +0200

    Add currency format for result value

--- src/components/ResultsCard/ResultCard.tsx
+++ src/components/ResultsCard/ResultCard.tsx
@@ -4,10 +4,12 @@ import {ChartPie} from './ResultChartPie';
 import {ResultCardContainer, TextResult, TextBold} from './ResultCard.styles';
 
 export const ResultCard = ({result, chartBarData, chartPieData}: any) => {
+    const formattedResult = new Intl.NumberFormat('pl-PL', {currency: 'PLN', style: 'currency'}).format(result);
+
     return (
         <ResultCardContainer>
             <TextResult>
-                Your total income will be <TextBold>{result} PLN</TextBold>
+                Your total income will be <TextBold>{formattedResult}</TextBold>
             </TextResult>
             <ChartBar data={chartBarData} />
             <ChartPie data={chartPieData} />
