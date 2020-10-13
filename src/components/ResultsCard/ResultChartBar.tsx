493cddb0baa4c2b2e1a399851ba36d1d31dfefb3
Michal Borowski <unpolacoo@gmail.com>
2020-10-14 00:51:32 +0200

    Correct keys in ChartBar

--- src/components/ResultsCard/ResultChartBar.tsx
+++ src/components/ResultsCard/ResultChartBar.tsx
@@ -1,33 +1,60 @@
 import React from 'react';
 import {ResponsiveBar} from '@nivo/bar';
 
-export const ChartBar = ({data}: any) => (
-    <ResponsiveBar
-        data={data}
-        keys={['startValue', 'cumulativeContribution', 'cumulativeInterest']}
-        indexBy="yearLabel"
-        margin={{top: 50, right: 130, bottom: 50, left: 60}}
-        padding={0.2}
-        colors={{scheme: 'nivo'}}
-        axisTop={null}
-        axisRight={null}
-        axisBottom={{
-            tickSize: 5,
-            tickPadding: 5,
-            tickRotation: 0,
-            legendPosition: 'middle',
-            legendOffset: 32,
-        }}
-        axisLeft={{
-            tickSize: 5,
-            tickPadding: 5,
-            tickRotation: 0,
-            legendPosition: 'middle',
-            legendOffset: -40,
-        }}
-        enableLabel={false}
-        animate={true}
-        motionStiffness={90}
-        motionDamping={15}
-    />
-);
+interface ChartData {
+    [key: string]: string;
+}
+interface AnnualResultProps {
+    annualContribution?: number;
+    annualInterest?: number;
+    cumulativeContribution?: number;
+    cumulativeInterest?: number;
+    id?: string;
+    startValue?: number;
+    startYearValue?: number;
+    year?: number;
+    yearLabel?: string;
+}
+
+export const ChartBar: React.FC<any> = ({data}) => {
+    console.log(data);
+
+    const chartData = data.map((el: ChartData) => {
+        return {
+            'Start Value': el.startValue,
+            'Cumulative Contribution': el.cumulativeContribution,
+            'Cumulative Interest': el.cumulativeInterest,
+            'Year Label': el.yearLabel,
+        };
+    });
+    return (
+        <ResponsiveBar
+            data={chartData}
+            keys={['Start Value', 'Cumulative Contribution', 'Cumulative Interest']}
+            indexBy="Year Label"
+            margin={{top: 50, right: 130, bottom: 50, left: 60}}
+            padding={0.2}
+            colors={{scheme: 'nivo'}}
+            axisTop={null}
+            axisRight={null}
+            axisBottom={{
+                tickSize: 5,
+                tickPadding: 5,
+                tickRotation: 0,
+                legendPosition: 'middle',
+                legendOffset: 32,
+            }}
+            axisLeft={{
+                tickSize: 5,
+                tickPadding: 5,
+                tickRotation: 0,
+                legendPosition: 'middle',
+                legendOffset: -40,
+            }}
+            enableLabel={false}
+            animate={true}
+            motionStiffness={90}
+            motionDamping={15}
+        />
+    );
+};
