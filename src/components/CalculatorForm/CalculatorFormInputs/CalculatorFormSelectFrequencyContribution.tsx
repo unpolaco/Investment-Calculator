b22aaf08bce22063a27309ae66575923b3524851
Michal Borowski <unpolacoo@gmail.com>
2020-10-15 01:07:44 +0200

    Correct styles

--- src/components/CalculatorForm/CalculatorFormInputs/CalculatorFormSelectFrequencyContribution.tsx
+++ src/components/CalculatorForm/CalculatorFormInputs/CalculatorFormSelectFrequencyContribution.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import {Field, useField} from 'formik';
+import {Select, Label, Fieldset} from './CalculatorFormInputs.styles';
 
 const dropdownOptions = [
     {key: 'Weekly', value: '52'},
@@ -10,15 +11,15 @@ const dropdownOptions = [
 export const CalculatorSelectFrequencyContribution: React.FC = () => {
     const [field] = useField('frequencyContribution');
     return (
-        <fieldset>
-            <label htmlFor={field.name}>Frequency Contribution</label>
-            <Field as="select" id={field.name} name={field.name}>
+        <Fieldset>
+            <Label htmlFor={field.name}>Frequency Contribution</Label>
+            <Field as={Select} id={field.name} name={field.name}>
                 {dropdownOptions.map(option => (
                     <option key={option.value} value={option.value}>
                         {option.key}
                     </option>
                 ))}
             </Field>
-        </fieldset>
+        </Fieldset>
     );
 };
--- src/resetStyles.ts
+++ src/resetStyles.ts
@@ -1,8 +1,11 @@
 export const resetStyles = `
-* {
+*, 
+*::before,
+*::after {
 	margin: 0;
 	padding: 0;
 	border: 0;
+	box-sizing: border-box;
 	font-size: 100%;
 	font-family: 'Lato', sans-serif;
 	vertical-align: baseline;
