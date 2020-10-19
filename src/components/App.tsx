4b2c0519acaa7ec098a07dafd3d20ebef61c1873
Michal Borowski <unpolacoo@gmail.com>
2020-10-19 21:51:41 +0200

    Create basic routing

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,14 +1,29 @@
 import React from 'react';
 import {ProductsPage} from './ProductsPage/ProductsPage';
+import {CalculatorForm} from './CalculatorPage/CalculatorForm/CalculatorForm';
 import {Main} from './App.styles';
 import {Header} from './Header/Header';
+import {BrowserRouter, Redirect, Route, Switch} from 'react-router-dom';
+import {ROUTES} from '../helpers/routes';
+import {getBaseName} from './App.helpers';
 export const App = () => {
+    const basename = getBaseName();
     return (
-        <>
+        <BrowserRouter basename={basename}>
             <Header />
             <Main>
-                <ProductsPage />
+                <Switch>
+                    <Route path={'/'} exact>
+                        <Redirect to={ROUTES.CALCULATOR} />
+                    </Route>
+                    <Route path={ROUTES.CALCULATOR}>
+                        <CalculatorForm />
+                    </Route>
+                    <Route path={ROUTES.PRODUCTS}>
+                        <ProductsPage />
+                    </Route>
+                </Switch>
             </Main>
-        </>
+        </BrowserRouter>
     );
 };
---
+++ src/helpers/routes.ts
@@ -0,0 +1,5 @@
+export enum ROUTES {
+    HOME = '/',
+    CALCULATOR = '/calculator',
+    PRODUCTS = '/products',
+}
