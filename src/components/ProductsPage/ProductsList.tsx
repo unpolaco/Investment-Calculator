0125f7450c64bc650271ca3d3577ca69018d5b19
Michal Borowski <unpolacoo@gmail.com>
2020-10-19 12:38:11 +0200

    Create useGetProducts hook (endpoint doesn't working)

--- src/components/ProductsPage/ProductsList.tsx
+++ src/components/ProductsPage/ProductsList.tsx
@@ -1,21 +1,34 @@
-import React from 'react';
+import React, {useEffect} from 'react';
+import {useGetProducts} from '../../hooks/useGetProducts';
 import {ProductItemCard} from './ProductItemCard';
 import {ProductsListContainer} from './ProductsList.styles';
+import {Loader} from '../../helpers/components/Loader/Loader';
 
 export const ProductsList = () => {
+    const {isFetching, getProductList, productList, isError} = useGetProducts();
+    useEffect(() => {
+        getProductList();
+    }, [getProductList]);
+
+    if (isFetching) {
+        return <Loader />;
+    }
+    if (isError) {
+        return alert('Error');
+    }
     return (
         <>
             <ProductsListContainer>
-                <ProductItemCard risk="1" rating="3" />
-                <ProductItemCard risk="3" rating="5" />
-                <ProductItemCard risk="5" rating="2" />
-                <ProductItemCard risk="2" rating="4" />
-                <ProductItemCard risk="5" rating="1" />
-                <ProductItemCard risk="1" rating="5" />
-                <ProductItemCard risk="1" rating="3" />
-                <ProductItemCard risk="4" rating="2" />
-                <ProductItemCard risk="6" rating="5" />
-                <ProductItemCard risk="3" rating="2" />
+                <ProductItemCard risk="1" rating="3" category="dłużny" />
+                <ProductItemCard risk="3" rating="5" category="mieszany" />
+                <ProductItemCard risk="5" rating="2" category="akcyjny" />
+                <ProductItemCard risk="2" rating="4" category="mieszany" />
+                <ProductItemCard risk="5" rating="1" category="dłużny" />
+                <ProductItemCard risk="1" rating="5" category="mieszany" />
+                <ProductItemCard risk="1" rating="3" category="akcyjny" />
+                <ProductItemCard risk="4" rating="2" category="akcyjny" />
+                <ProductItemCard risk="6" rating="5" category="dłużny" />
+                <ProductItemCard risk="3" rating="2" category="akcyjny" />
             </ProductsListContainer>
         </>
     );
