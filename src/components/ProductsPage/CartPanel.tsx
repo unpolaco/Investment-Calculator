34fd606a56e7b50f582936453ac5d94cf529ee27
Michal Borowski <unpolacoo@gmail.com>
2020-10-21 10:40:07 +0200

    Create empty cart box

--- src/components/ProductsPage/CartPanel.tsx
+++ src/components/ProductsPage/CartPanel.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
-import {CartPanelContainer} from './CartPanel.styles';
+import {CartPanelContainer, EmptyCart} from './CartPanel.styles';
 import {CartPanelInput} from './CartPanelInput';
-export const CartPanel = () => {
+export const CartPanel: React.FC<any> = () => {
     return (
         <CartPanelContainer>
             <CartPanelInput />
+            <EmptyCart>Click on the list on the right side to add product to the cart</EmptyCart>
         </CartPanelContainer>
     );
 };
