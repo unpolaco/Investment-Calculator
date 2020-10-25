894ea4f1170adc0ee7a4872347f4f32d3057bcc5
Michal Borowski <unpolacoo@gmail.com>
2020-10-25 14:44:36 +0100

    Style CartPanelProductItem

--- src/components/ProductsPage/CartPanelIProductItem.tsx
+++ src/components/ProductsPage/CartPanelIProductItem.tsx
@@ -1,25 +1,29 @@
 import React from 'react';
 import {Field} from 'formik';
-import {Input, Label, Fieldset} from './CartPanelProductItem.styles';
+import {Input, Fieldset, FieldWrapper, PercentQuota, ProductName, FieldContainerCurrency} from './CartPanelProductItem.styles';
 
-export const CartPanelProductItem: React.FC<any> = ({name, id, handleBlur, calculateProducts, values}) => {
+export const CartPanelProductItem: React.FC<any> = ({name, id, handleBlur, calculateProducts, values, percentQuota}) => {
     return (
         <div key={id}>
             <Fieldset>
-                {name}
-                <Label>Enter amount</Label>
-                <Field
-                    autoFocus
-                    autoComplete="off"
-                    type="number"
-                    as={Input}
-                    name={name}
-                    value={values.name}
-                    onBlur={(e: any) => {
-                        handleBlur(e);
-                        calculateProducts(values);
-                    }}
-                />
+                <ProductName>{name}</ProductName>
+                <FieldWrapper>
+                    <FieldContainerCurrency>
+                        <Field
+                            autoFocus
+                            autoComplete="off"
+                            type="number"
+                            as={Input}
+                            name={name}
+                            value={values.name}
+                            onBlur={(e: any) => {
+                                handleBlur(e);
+                                calculateProducts(values);
+                            }}
+                        />
+                    </FieldContainerCurrency>
+                    <PercentQuota>/ {percentQuota || 0}%</PercentQuota>
+                </FieldWrapper>
             </Fieldset>
         </div>
     );
