81eb5da0d657691d0621aaf30ef3037dc2a105c7
Michal Borowski <unpolacoo@gmail.com>
2020-10-25 14:35:01 +0100

    Create GlobalWrapper with min and max- width

--- src/components/App.styles.ts
+++ src/components/App.styles.ts
@@ -1,5 +1,9 @@
 import styled from '@emotion/styled';
 
+export const GlobalWrapper = styled.div`
+    min-width: 800px;
+    max-width: 1500px;
+`;
 export const Main = styled.main`
     display: flex;
     height: calc(100vh - 120px);
